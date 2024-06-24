298e645f13678bd4225cc0acb47df04a36669201
Mathew <mathewjosef41@gmail.com>
2024-06-24 10:49:00 +0530

    Test commit

--- BackEnd/djact/djact/settings.py
+++ BackEnd/djact/djact/settings.py
@@ -93,7 +93,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'static/dist')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -217,6 +217,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 MEDIA_URL= '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR ,'media')
--- BackEnd/djact/djact/urls.py
+++ BackEnd/djact/djact/urls.py
@@ -19,11 +19,14 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
     TokenVerifyView,    
 )
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
