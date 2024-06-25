19bf6d5bd8da66634de2fbd5e5dc04bd28a8901e
Mathew <mathewjosef41@gmail.com>
2024-06-25 10:10:14 +0530

    Render deployment

---
+++ BackEnd/djact/build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# Exit on error
+set -o errexit
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Collect static files
+python manage.py collectstatic --no-input
+
+# Apply database migrations
+python manage.py migrate
