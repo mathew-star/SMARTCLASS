264387a688049ae142fbf1e9c8f906791a5f51ed
Mathew <mathewjosef41@gmail.com>
2024-06-26 03:54:01 +0530

    fix: redis updated for celery

--- BackEnd/djact/djact/settings.py
+++ BackEnd/djact/djact/settings.py
@@ -38,8 +38,8 @@ ALLOWED_HOSTS = ['*']
 
 
 # Celery settings
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_BROKER_URL = 'redis://red-cpshadd6l47c73e5g5ng:6379'
+CELERY_RESULT_BACKEND = 'redis://red-cpshadd6l47c73e5g5ng:6379'
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
